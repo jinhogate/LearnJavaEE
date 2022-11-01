cf474154a7eeda55991ad803491172e3a3c4654c
GBEGNON kossi <kossijorjinho.gbegnon@gmail.com>
2022-11-01 19:43:16 +0100

    Création du bean AuteurBean, le modèle

---
+++ src/main/java/com/learnjavaee/models/beans/AuteurBean.java
@@ -0,0 +1,28 @@
+package com.learnjavaee.models.beans;
+
+public class AuteurBean {
+	
+	private String name;
+	private String firstName;
+	private Boolean actif;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public Boolean getActif() {
+		return actif;
+	}
+	public void setActif(Boolean actif) {
+		this.actif = actif;
+	}
+
+}
