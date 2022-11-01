b3e385fcda297894f8b50e98829e2d46ad68b285
GBEGNON kossi <kossijorjinho.gbegnon@gmail.com>
2022-11-01 18:57:07 +0100

    Refactoring

--- src/main/java/com/learnjavaee/controler/servlets/LearnJavaServlet.java
+++ src/main/java/com/learnjavaee/controler/servlets/LearnJavaServlet.java
@@ -13,33 +13,39 @@ import java.io.PrintWriter;
  */
 public class LearnJavaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LearnJavaServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 * Cette méthode devrait recevoir la requéte HTTP en paramètre, effecturer une action puis renvoyer une réponse
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public LearnJavaServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response) Cette méthode devrait recevoir la requéte HTTP en paramètre,
+	 *      effecturer une action puis renvoyer une réponse
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// Echange de données du servlet au JSPs
 		request.setAttribute("userName", "GBEGNON Kossi");
 		request.setAttribute("userOld", 25);
-		// On recupère dans un premiere temps la référence de notre servlet dans le context courant dans lequel il est exécuté
-		// Ensuite on recupère l'object RequestDispatcher qui permet de lier une requête à une ressource
+		// On recupère dans un premiere temps la référence de notre servlet dans le
+		// context courant dans lequel il est exécuté
+		// Ensuite on recupère l'object RequestDispatcher qui permet de lier une requête
+		// à une ressource
 		// Enfin on applique la redirection
 		this.getServletContext().getRequestDispatcher("/WEB-INF/pages/learn.jsp").forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
